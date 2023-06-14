e97193e339be391cb273528b0c8dfc19d8ae08d0
Ramón García Verjaga <ramongarver2000@gmail.com>
2023-06-14 11:30:34 +0200

    ⚠️ Implement global exception handler. Closes #17

---
+++ api/src/main/java/com/ramongarver/poppy/api/exception/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.ramongarver.poppy.api.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<Object> handleResourceNotFoundException(ResourceNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(EmailExistsException.class)
+    public ResponseEntity<Object> handleEmailExistsException(EmailExistsException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.CONFLICT);
+    }
+
+}
