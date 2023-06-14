1627abcacec0172b7250b2d6639358f6cc69fe47
Ramón García Verjaga <ramongarver2000@gmail.com>
2023-06-14 11:25:50 +0200

    🔍 Create new API endpoint for retrieving user information

--- api/src/main/java/com/ramongarver/poppy/api/auth/AuthenticationController.java
+++ api/src/main/java/com/ramongarver/poppy/api/auth/AuthenticationController.java
@@ -1,8 +1,11 @@
 package com.ramongarver.poppy.api.auth;
 
 import com.ramongarver.poppy.api.controller.ControllerConstants;
+import com.ramongarver.poppy.api.dto.UserDto;
+import com.ramongarver.poppy.api.mapper.UserMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(ControllerConstants.AUTH_ROUTE)
 public class AuthenticationController {
+
+    private final UserMapper userMapper;
 
     private final AuthenticationService authService;
 
@@ -27,6 +32,11 @@ public class AuthenticationController {
             @RequestBody AuthenticationRequest request
     ) {
         return ResponseEntity.ok(authService.authenticate(request));
+    }
+
+    @GetMapping(ControllerConstants.AUTH_MY_ACCOUNT_ROUTE)
+    public ResponseEntity<UserDto> myAccount() {
+        return ResponseEntity.ok(userMapper.toDto(authService.getAuthenticatedUser()));
     }
 
 }
