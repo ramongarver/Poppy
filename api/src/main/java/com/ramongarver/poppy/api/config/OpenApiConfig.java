38cf276b361866732ab05114cdd9c8473dcbb556
Ramón García Verjaga <ramongarver2000@gmail.com>
2023-06-14 11:14:31 +0200

    📚 Add configuration to generate OpenAPI doc. Closes #16

---
+++ api/src/main/java/com/ramongarver/poppy/api/config/OpenApiConfig.java
@@ -0,0 +1,25 @@
+package com.ramongarver.poppy.api.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI openApi() {
+        return new OpenAPI().info(new Info()
+                .title("Poppy API")
+                .summary("API REST for the ESN management application Poppy")
+                .description("")
+                .termsOfService("http://swagger.io/terms/")
+                .contact(new Contact().name("Ramón García Verjaga").url("").email(""))
+                .license(new License().name("Copyright © 2023 Ramón García Verjaga").url(""))
+                .version("0.1.0"));
+    }
+
+}
