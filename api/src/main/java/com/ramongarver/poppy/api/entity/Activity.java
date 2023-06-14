aeb4871f7ff94a0fd4fe1ff59e985e35f6c2f67c
Ramón García Verjaga <ramongarver2000@gmail.com>
2023-06-14 11:35:33 +0200

    📋 Create Activity entity

---
+++ api/src/main/java/com/ramongarver/poppy/api/entity/Activity.java
@@ -0,0 +1,50 @@
+package com.ramongarver.poppy.api.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Activity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private LocalDateTime localDateTime;
+
+    @ManyToMany
+    @JoinTable(
+            name = "activity_volunteer",
+            joinColumns = @JoinColumn(name = "activity_id"),
+            inverseJoinColumns = @JoinColumn(name = "volunteer_id")
+    )
+    private List<Volunteer> volunteers = new ArrayList<>();
+
+}
