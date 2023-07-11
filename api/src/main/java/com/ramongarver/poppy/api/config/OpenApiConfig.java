6ff8550058b951f7a5b57308d567a881267a52b1
Ramón García Verjaga <ramongarver2000@gmail.com>
2023-07-11 17:49:50 +0200

    🔑 Add information related to authentication in the OpenAPI spec

--- api/src/main/java/com/ramongarver/poppy/api/config/OpenApiConfig.java
+++ api/src/main/java/com/ramongarver/poppy/api/config/OpenApiConfig.java
@@ -1,9 +1,11 @@
 package com.ramongarver.poppy.api.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,14 +14,20 @@ public class OpenApiConfig {
 
     @Bean
     public OpenAPI openApi() {
-        return new OpenAPI().info(new Info()
+        return new OpenAPI()
+            .info(new Info()
                 .title("Poppy API")
                 .summary("API REST for the ESN management application Poppy")
                 .description("")
                 .termsOfService("http://swagger.io/terms/")
                 .contact(new Contact().name("Ramón García Verjaga").url("").email(""))
                 .license(new License().name("Copyright © 2023 Ramón García Verjaga").url(""))
-                .version("0.1.0"));
+                .version("0.1.0"))
+            .components(new Components()
+                .addSecuritySchemes("JWT Bearer Authentication", new SecurityScheme()
+                    .type(SecurityScheme.Type.HTTP)
+                    .scheme("bearer")
+                    .bearerFormat("JWT")));
     }
 
 }
