2ae141dd00b8f0605248293858c91987b52be5be
Ramón García Verjaga <ramongarver2000@gmail.com>
2023-07-13 13:39:37 +0200

    ✏️ Migrate script to English

--- scripts/data/volunteer_generator.py
+++ scripts/data/volunteer_generator.py
@@ -3,8 +3,8 @@ import sys
 
 import numpy as np
 
-# Nombres de mujeres
-nombres_mujeres = np.array([
+# Women's names
+women_names = np.array([
     'MARIA CARMEN', 'MARIA', 'CARMEN', 'ANA MARIA', 'LAURA', 'MARIA PILAR', 'MARIA DOLORES', 'ISABEL',
     'JOSEFA', 'MARIA TERESA', 'ANA', 'MARTA', 'CRISTINA', 'MARIA ANGELES', 'LUCIA', 'MARIA ISABEL',
     'MARIA JOSE', 'FRANCISCA', 'ANTONIA', 'DOLORES', 'PAULA', 'SARA', 'ELENA', 'MARIA LUISA', 'RAQUEL',
@@ -19,8 +19,8 @@ nombres_mujeres = np.array([
     'AINHOA', 'OLGA', 'CLARA', 'CONSUELO', 'MARIA CRISTINA', 'ADRIANA', 'GLORIA', 'MARIA SOLEDAD'
 ])
 
-# Nombres de hombres
-nombres_hombres = np.array([
+# Men's names
+men_names = np.array([
     'ANTONIO', 'MANUEL', 'JOSE', 'FRANCISCO', 'DAVID', 'JUAN', 'JAVIER', 'DANIEL', 'JOSE ANTONIO',
     'FRANCISCO JAVIER', 'JOSE LUIS', 'CARLOS', 'ALEJANDRO', 'JESUS', 'MIGUEL', 'JOSE MANUEL',
     'MIGUEL ANGEL', 'RAFAEL', 'PABLO', 'PEDRO', 'ANGEL', 'SERGIO', 'FERNANDO', 'JOSE MARIA',
@@ -36,8 +36,8 @@ nombres_hombres = np.array([
     'JUAN LUIS'
 ])
 
-# Apellidos
-apellidos = np.array([
+# Last names
+last_names = np.array([
     'GARCIA', 'RODRIGUEZ', 'GONZALEZ', 'FERNANDEZ', 'LOPEZ', 'MARTINEZ', 'SANCHEZ', 'PEREZ',
     'GOMEZ', 'MARTIN', 'JIMENEZ', 'HERNANDEZ', 'RUIZ', 'DIAZ', 'MORENO', 'ALVAREZ',
     'ROMERO', 'GUTIERREZ', 'ALONSO', 'NAVARRO', 'TORRES', 'DOMINGUEZ', 'RAMOS', 'VAZQUEZ',
@@ -60,64 +60,63 @@ apellidos = np.array([
     'MALDONADO', 'SIMON', 'ALARCON', 'ZAMORA', 'MILLAN', 'PAREDES', 'LAZARO'
 ])
 
-# Verificar si se proporcionó un argumento para activar el atributo de género
-generar_genero = False
+# Check if an argument was provided to activate the gender attribute
+generate_gender = False
 if len(sys.argv) > 1 and sys.argv[1].lower() == 'gender=true':
-    generar_genero = True
+    generate_gender = True
 
+# Generate a random value for firstName in the format "Name"
+def generate_name():
+    names = np.concatenate((women_names, men_names))
+    name = np.random.choice(names)
+    name = name.title().split(" ")[0]  # Convert first letter to uppercase and get first name
+    return name
 
-# Generar un valor aleatorio para firstName en el formato "Nombre"
-def generar_nombre():
-    nombres = np.concatenate((nombres_mujeres, nombres_hombres))
-    nombre = np.random.choice(nombres)
-    nombre = nombre.title().split(" ")[0]  # Convertir primera letra en mayúscula y obtener primer nombre
-    return nombre
+# Generate a random value for lastName in the format "LastName1 LastName2"
+def generate_last_name():
+    last_name1 = np.random.choice(last_names).title()
+    last_name2 = np.random.choice(last_names).title()
+    last_name = last_name1 + " " + last_name2
+    return last_name
 
-# Generar un valor aleatorio para lastName en el formato "Apellido1 Apellido2"
-def generar_apellido():
-    apellido1 = np.random.choice(apellidos).title()
-    apellido2 = np.random.choice(apellidos).title()
-    apellido = apellido1 + " " + apellido2
-    return apellido
-
-# Generar un valor aleatorio para startDate en el formato "YYYY-MM-DD"
-def generar_start_date():
+# Generate a random value for startDate in the format "YYYY-MM-DD"
+def generate_start_date():
     year = np.random.choice([2021, 2022])
     month = np.random.choice([2, 9])
     start_date = f"{year:04d}-{month:02d}-01"
     return start_date
 
-# Generar un número N de instancias diferentes
-N = 100  # Número de instancias a generar
+# Generate a number N of different instances
+N = 100  # Number of instances to generate
 
-volunteers = []  # Lista para almacenar las instancias de voluntarios
+volunteers = []  # List to store volunteer instances
 
 for _ in range(N):
-    # Generar una instancia de voluntario
-    nombre = generar_nombre()
-    apellido = generar_apellido()
-    start_date = generar_start_date()
-    email = f"{nombre.lower()}.{apellido.split()[0].lower()}@poppy-test.com"
+    # Generate a volunteer instance
+    name = generate_name()
+    last_name = generate_last_name()
+    start_date = generate_start_date()
+    email = f"{name.lower()}.{last_name.split()[0].lower()}@poppy-test.com"
 
-    # Crear el objeto JSON
+    # Create the JSON object
     volunteer = {
-        "firstName": nombre,
-        "lastName": apellido,
+        "firstName": name,
+        "lastName": last_name,
         "email": email,
         "password": "",
         "role": "USER",
         "startDate": start_date
     }
 
-    # Agregar el atributo de género si está habilitado
-    if generar_genero:
-        gender = "FEMALE" if nombre.upper() in nombres_mujeres else "MALE"
+    # Add the gender attribute if enabled
+    if generate_gender:
+        gender = "FEMALE" if name.upper() in women_names else "MALE"
         volunteer["gender"] = gender
 
-    volunteers.append(volunteer)  # Agregar el objeto a la lista de voluntarios
+    volunteers.append(volunteer)  # Add the object to the volunteers list
 
-# Guardar las instancias de voluntarios en un archivo JSON
+# Save the volunteer instances to a JSON file
 with open("volunteers.json", "w") as file:
     json.dump(volunteers, file, indent=4)
 
-print("Archivo 'volunteers.json' generado con éxito.")
+print("File 'volunteers.json' successfully generated.")
