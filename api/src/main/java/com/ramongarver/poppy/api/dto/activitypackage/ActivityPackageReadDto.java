b88c76cb083965a67593987a5258ae09026936fc
Ramón García Verjaga <ramongarver2000@gmail.com>
2023-08-02 12:40:44 +0200

    ↔️ Add activity package dtos and mapper

---
+++ api/src/main/java/com/ramongarver/poppy/api/dto/activitypackage/ActivityPackageReadDto.java
@@ -0,0 +1,38 @@
+package com.ramongarver.poppy.api.dto.activitypackage;
+
+import com.ramongarver.poppy.api.enums.ActivityPackageType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ActivityPackageReadDto {
+
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private ActivityPackageType type;
+
+    private LocalDate availabilityStartDate;
+
+    private LocalDate availabilityEndDate;
+
+    private boolean isVisible;
+
+    private List<Long> activityIds;
+
+    private boolean areVolunteersAssigned;
+
+}
