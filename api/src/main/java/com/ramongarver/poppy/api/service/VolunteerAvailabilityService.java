f33b49530ffab73bd60900ef46886aaa51528919
Ramón García Verjaga <ramongarver2000@gmail.com>
2023-08-07 16:46:37 +0200

    📆 Add volunteer availability service

---
+++ api/src/main/java/com/ramongarver/poppy/api/service/VolunteerAvailabilityService.java
@@ -0,0 +1,21 @@
+package com.ramongarver.poppy.api.service;
+
+import com.ramongarver.poppy.api.dto.volunteeravailability.VolunteerAvailabilityCreateDto;
+import com.ramongarver.poppy.api.dto.volunteeravailability.VolunteerAvailabilityUpdateDto;
+import com.ramongarver.poppy.api.entity.VolunteerAvailability;
+
+import java.util.List;
+
+public interface VolunteerAvailabilityService {
+
+    VolunteerAvailability createActivityPackageVolunteerAvailability(Long activityPackageId, Long volunteerId, VolunteerAvailabilityCreateDto volunteerAvailabilityCreateDto);
+
+    VolunteerAvailability getVolunteerAvailabilityByActivityPackageIdAndVolunteerId(Long activityPackageId, Long volunteerId);
+
+    List<VolunteerAvailability> getAllVolunteerAvailabilitiesByActivityPackageId(Long activityPackageId);
+
+    VolunteerAvailability updateVolunteerAvailability(Long activityPackageId, Long volunteerId, VolunteerAvailabilityUpdateDto volunteerAvailabilityUpdateDto);
+
+    void deleteVolunteerAvailability(Long activityPackageId, Long volunteerId);
+
+}
