f6e1809883916266a7e937dd15257eb41268fc4a
Ramón García Verjaga <ramongarver2000@gmail.com>
2023-09-01 19:13:12 +0200

    🔒 Complete description and health check in RootController

--- api/src/main/java/com/ramongarver/poppy/api/controller/RootController.java
+++ api/src/main/java/com/ramongarver/poppy/api/controller/RootController.java
@@ -1,5 +1,10 @@
 package com.ramongarver.poppy.api.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,11 +15,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @AllArgsConstructor
 @RequestMapping(ControllerConstants.ROOT_ROUTE)
+@Tag(name = "Root", description = "Health check and basic information")
 public class RootController {
 
     @GetMapping
-    public ResponseEntity<String> getActivityById() {
-        return new ResponseEntity<>("Good new to see this! :)", HttpStatus.OK);
+    @Operation(summary = "Get a basic message",
+            responses = {
+                    @ApiResponse(responseCode = "200", content = @Content(schema = @Schema(implementation = String.class)))
+            }
+    )
+    public ResponseEntity<String> getMessage() {
+        return new ResponseEntity<>("Good new to see this, I'm working! :)", HttpStatus.OK);
     }
 
 }
