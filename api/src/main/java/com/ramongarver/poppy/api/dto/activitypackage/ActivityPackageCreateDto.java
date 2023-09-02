b02e3fbdd5ea675465ab88a4c9b8ac4cac61c0a3
Ramón García Verjaga <ramongarver2000@gmail.com>
2023-09-02 10:49:35 +0200

    🧪 Fix code smells

--- api/src/main/java/com/ramongarver/poppy/api/dto/activitypackage/ActivityPackageCreateDto.java
+++ api/src/main/java/com/ramongarver/poppy/api/dto/activitypackage/ActivityPackageCreateDto.java
@@ -1,7 +1,6 @@
 package com.ramongarver.poppy.api.dto.activitypackage;
 
 import com.ramongarver.poppy.api.enums.ActivityPackageType;
-import jakarta.persistence.Column;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
